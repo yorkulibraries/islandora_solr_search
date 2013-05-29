240eda4e85aa5a05f8c80d111eeacc1f7ac5bb4c
phil <phil@greybush.(none)>
2013-05-29 13:53:11 -0300

    updated api.php to meet coding standards

--- islandora_solr_search.api.php
+++ islandora_solr_search.api.php
@@ -1,26 +1,22 @@
 <?php
 /**
  * @file
- *   This file defines the hooks that islandora_solr_search
- *   makes available.
+ * This file defines the hooks that islandora_solr_search
+ * makes available.
  */
 
 /**
  * This hook allows modules to edit an rss_item.
- * Override rss Item.
- * (from islandora_solr_search/islandora_solr_config
  *
  * Somtimes you might want to alter how an rss item is displayed.
  *
  * @param rssItem $item
  *   The rssItem object
- *
- * @param $doc
+ * @param array $doc
  *   The solr results document
- *
  */
 function hook_islandora_solr_search_rss_item_alter($item, $doc) {
 
-  $item['title'] =  $doc['PID'];
+  $item['title'] = $doc['PID'];
   $item['description'] = 'this is the new rss item description';
 }
